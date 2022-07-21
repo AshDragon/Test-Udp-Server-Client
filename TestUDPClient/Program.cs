532d609fec07cb9f34d236580c8928f5f23e48dd
AshDragon <magastrala@yandex.ru>
2022-07-21 12:24:04 +0400

    add Test-Udp-Server-Client

---
+++ TestUDPClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUDPClient
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            IPAddress serverIP = IPAddress.Parse("127.0.0.1");
+                       
+                // Создаем UdpClient
+                UdpClient udpClient = new UdpClient();
+
+                // Соединяемся с удаленным хостом
+                udpClient.Connect(serverIP, 5555);
+
+                string message = "Hello Server";
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                int numberOfSentBytes = udpClient.Send(data, data.Length);
+                udpClient.Close();
+
+                Console.ReadKey();
+            }
+    }
+}
---
+++ TestUDPServer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace TestUDPServer
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int recv;
+            byte[] data = new byte[1024];
+
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, 5555);
+
+
+            Socket newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            newSocket.Bind(endpoint);
+
+            Console.WriteLine(" Waiting for client connection ...");
+
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 5555);  
+            EndPoint tmpRemote = (EndPoint)sender; 
+
+            recv = newSocket.ReceiveFrom(data, ref tmpRemote);
+
+            Console.Write("Message received from {0}: ", tmpRemote.ToString ());
+            Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+
+
+
+            string welcom = "Welcome to my TestServer!";
+            data = Encoding.ASCII.GetBytes(welcom);
+
+
+            if (newSocket.Connected)
+                newSocket.Send(data); 
+
+            while(true)
+            {
+                if(newSocket.Connected)
+                {
+                    Console.WriteLine("Client Disconnected.");
+                    break;
+                }
+
+                data = new byte[1024];
+                recv = newSocket.ReceiveFrom(data, ref tmpRemote);
+
+                if (recv == 0)
+                {
+                    break;
+                }
+                Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));
+            }
+
+            newSocket.Close();
+            
+        }
+    }
+}
+    
+
